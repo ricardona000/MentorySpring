01096e3dfc0d0b4cf0f3682b0508cc61c8cca5fc
ricardona17 <ricardona17@gmail.com>
2021-04-10 17:51:50 -0500

    Section 3, Step 6 @Primary annotation

--- spring/src/main/java/mentory/spring/QuickSortAlgorithm.java
+++ spring/src/main/java/mentory/spring/QuickSortAlgorithm.java
@@ -1,5 +1,10 @@
 package mentory.spring;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class QuickSortAlgorithm implements SortAlgorithm {
 
     public int[] sort(int[] numbers) {

f5723a8d746f89a72a967ef3bd51d07e3ffcc885
ricardona17 <ricardona17@gmail.com>
2021-04-22 21:08:56 -0500

    Section 4, Step 23 Mixing XML Context with Component Scan

--- spring/src/main/java/mentory/spring/XmlContextApplication.java
+++ spring/src/main/java/mentory/spring/XmlContextApplication.java
@@ -1,21 +1,22 @@
 package mentory.spring;
 
 import mentory.spring.xml.XmlPersonDAO;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@Configuration
-@ComponentScan
 public class XmlContextApplication {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(XmlContextApplication.class);
 
 	public static void main(String[] args) {
 
 		// Application Context
         try (ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml")) {
+            LOGGER.info("Beans Loaded -> {}", (Object) applicationContext.getBeanDefinitionNames());
             XmlPersonDAO xmlPersonDAO = applicationContext.getBean(XmlPersonDAO.class);
-            System.out.println("xmlPersonDAO = " + xmlPersonDAO);
-            System.out.println("xmlJdbcConnection = " + xmlPersonDAO.getXmlJdbcConnection());
+            LOGGER.info("xmlPersonDAO = {}", xmlPersonDAO);
+            LOGGER.info("xmlJdbcConnection = {}", xmlPersonDAO.getXmlJdbcConnection());
         }
 
 		//applicationContext.close();

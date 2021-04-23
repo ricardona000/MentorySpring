579c231b631219261eda52854bf05bc6842a36e7
ricardona17 <ricardona17@gmail.com>
2021-04-22 20:52:34 -0500

    Section 4, Step 21 Defining Spring Application Context using XML

---
+++ spring/src/main/java/mentory/spring/xml/XmlJdbcConnection.java
@@ -0,0 +1,9 @@
+package mentory.spring.xml;
+
+public class XmlJdbcConnection {
+
+    public XmlJdbcConnection() {
+        System.out.println("XML JDBC Connection");
+    }
+
+}
---
+++ spring/src/main/java/mentory/spring/xml/XmlPersonDAO.java
@@ -0,0 +1,14 @@
+package mentory.spring.xml;
+
+public class XmlPersonDAO {
+
+    private XmlJdbcConnection xmlJdbcConnection;
+
+    public XmlJdbcConnection getXmlJdbcConnection() {
+        return xmlJdbcConnection;
+    }
+
+    public void setXmlJdbcConnection(XmlJdbcConnection xmlJdbcConnection) {
+        this.xmlJdbcConnection = xmlJdbcConnection;
+    }
+}

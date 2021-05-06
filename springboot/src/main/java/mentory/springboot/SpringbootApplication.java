25b59e4f0a18c05fb19c9ffae0d67d5f01f6a7be
ricardona17 <ricardona17@gmail.com>
2021-05-05 20:45:40 -0500

    Section 8 - Step 5 Auto Configuration

--- springboot/src/main/java/mentory/springboot/SpringbootApplication.java
+++ springboot/src/main/java/mentory/springboot/SpringbootApplication.java
@@ -2,12 +2,18 @@ package mentory.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+// This annotation enables: Spring Context, Auto Configuration, Component Scan
 @SpringBootApplication
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(SpringbootApplication.class, args);
+
+		for (String name : applicationContext.getBeanDefinitionNames()) {
+			System.out.println("Bean name: " + name);
+		}
 	}
 
 }
